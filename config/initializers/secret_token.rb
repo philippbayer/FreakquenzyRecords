7d6766dc2e8769e4f2e8620ddb3cda4416c43173
Philipp <drsnuggles@Philipp.(none)>
2011-05-26 09:49:35 +1000

    And here we go.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreakquenzyRecords::Application.config.secret_token = '2e4c21ba5c785b1685fc63df09d973258f57cdecb036c1c98ec55c19c95aac4f8f55807e51959d3b3792d47f53479dc3d0fca6dee7810ce491661e6bdaf7dea1'
