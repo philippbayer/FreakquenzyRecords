093fe4e97955381614a5026b87826d13e9da5a62
Philipp <PhilippBay@gmail.com>
2011-05-26 17:14:02 +1000

    Added new layout and show for every band/user

--- app/controllers/band_controller.rb
+++ app/controllers/band_controller.rb
@@ -1,10 +1,20 @@
 class BandController < ApplicationController
 
 	def index
+		# showing all bands
 		@bands = Band.find(:all)
+		@band_users = BandUser.find(:all)
 
-		# now find all users associated with each band
+		respond_to do |format|
+			format.html
+		end
+	end
 
+	def show
+		# showing a single band's page
+		
+		@band = Band.find(params[:id])
+		@band_users = BandUser.find(:all)
 
 		respond_to do |format|
 			format.html
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,23 @@
 class UserController < ApplicationController
+
 	def index
+		# showing all users
+		@users = User.find(:all)
+		@band_users = BandUser.find(:all)
+
 		respond_to do |format|
 			format.html
 			format.xml # just for the hell of it
+		end
+	end
+
+	def show
+		# showing a single user's page
+		@user = User.find(params[:id])
+		@band_users = BandUser.find(:all)
+
+		respond_to do |format|
+			format.html
 		end
 	end
 end
