5f07081c1f1bf85f108a4de1df172d6f1820f460
Philipp <PhilippBay@gmail.com>
2011-05-26 17:47:09 +1000

    added structure/root, added header

--- config/routes.rb
+++ config/routes.rb
@@ -61,5 +61,5 @@ FreakquenzyRecords::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-  root :to => "static#index"
+  root :to => "news#index"
 end
