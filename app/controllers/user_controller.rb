946c848d41bf7895760914651b4d1c80aee75597
Philipp <PhilippBay@gmail.com>
2011-05-28 12:26:37 +1000

    Started to add login/logout/password system

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,26 @@
 class UserController < ApplicationController
+    # let non-authenticated users only see show, new, create
+	before_filter :authenticate, :except => [:show, :new, :create]
+	# let only the corret user use edit and update
+	before_filter :correct_user, :only => [:edit, :update]
+
+	def new
+		@user = User.new
+		@title = "Sign up"
+	end
+
+	def create
+		@user = User.new(params[:id])
+		if @user.save
+			sign_in @user
+			flash[:success] = "Welcome to Freakquenzy Records!"
+			redirect_to @user
+		else
+			@title = "Sign up"
+			render 'new'
+		end
+	end
+
 
 	def index
 		# showing all users
@@ -20,4 +42,16 @@ class UserController < ApplicationController
 			format.html
 		end
 	end
+
+	private
+
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
+	end
+
+	def admin_user
+		redirect_to(root_path) unless current_user.admin?
+	end
+
 end
