5dc955eb1c9e93501954af39a1e6e20a038d0f32
drsnuggles <philippbay@gmail.com>
2012-12-12 19:14:50 +1000

    Proper alerts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
+    before_filter :admin_check, :only => [:admin]
     def index
         @users = User.all
     end
 
     def admin
-        admin_check
     end
 
     private
     def admin_check
         # send back to index except user is admin and logged in
         unless current_user.try("admin?")
+            flash[:alert] = "Hey"
             redirect_to root_path
         end
     end
